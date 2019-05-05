176efaa0daee7ccdc361710c44f7f64de1bd829e
Nordine Khelfi <nordine.khelfi@gmail.com>
2019-05-05 15:36:24 +0200

    - Migrated to AndroidX - Used ButterKnife library

--- app/src/main/java/com/example/topquizalpha/MainMenuActivity.java
+++ app/src/main/java/com/example/topquizalpha/MainMenuActivity.java
@@ -1,66 +1,44 @@
 package com.example.topquizalpha;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-import java.util.Arrays;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.List;
 
+import butterknife.BindViews;
+import butterknife.ButterKnife;
 import io.paperdb.Paper;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     public static final String TOPIC_EXTRA_KEY = "TOPIC";
-    public ImageButton ibProphets;
-    public ImageButton ibMuhammad;
-    public ImageButton ibQuran;
-    public ImageButton ibSalat;
-    public List<ImageButton> topicImageButtons;
+
+    @BindViews({R.id.ibProphets, R.id.ibMuhammad, R.id.ibQuran, R.id.ibSalat})
+        List<ImageButton> topicImageButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
         Paper.init(this);
-        Paper.book().write(String.valueOf(R.id.ibProphets), true);
-
-        ibProphets = findViewById(R.id.ibProphets);
-        ibMuhammad = findViewById(R.id.ibMuhammad);
-        ibQuran = findViewById(R.id.ibQuran);
-        ibSalat = findViewById(R.id.ibSalat);
-
-        topicImageButtons = Arrays.asList(
-                ibProphets,
-                ibMuhammad,
-                ibQuran,
-                ibSalat
-        );
-
-        for(ImageButton topic : topicImageButtons){
-            topic.setImageResource(Paper.book().read(String.valueOf(topic.getId()), false) ?
-                    R.drawable.ib_selector :
-                    R.drawable.mosque_bw);
-        }
+        RefreshMainMenu();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Paper.book().write(String.valueOf(R.id.ibProphets), true);
-
-        for(ImageButton topic : topicImageButtons){
-            topic.setImageResource(Paper.book().read(String.valueOf(topic.getId()), false) ?
-                    R.drawable.ib_selector :
-                    R.drawable.mosque_bw);
-        }
+        RefreshMainMenu();
     }
 
     public void onTopicClick(View view) {
+
         if (Paper.book().read(String.valueOf(view.getId()), false)) {
             Intent intent = new Intent(MainMenuActivity.this, PlayerActivity.class);
             intent.putExtra(TOPIC_EXTRA_KEY, view.getId());
@@ -70,13 +48,18 @@ public class MainMenuActivity extends AppCompatActivity {
             builder.setTitle("This topic is locked")
                     .setIcon(R.drawable.ic_mecca)
                     .setMessage("You must reach 100% of good answers at the previous topic first.")
-                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-
-                        }
-                    })
+                    .setPositiveButton("Okay", (dialog, which) -> dialog.dismiss())
                     .create().show();
+        }
+    }
+
+    private void RefreshMainMenu(){
+        Paper.book().write(String.valueOf(R.id.ibProphets), true);
+
+        for(ImageButton topic : topicImageButtons){
+            topic.setImageResource(Paper.book().read(String.valueOf(topic.getId()), false) ?
+                    R.drawable.ib_selector :
+                    R.drawable.mosque_bw);
         }
     }
 }
