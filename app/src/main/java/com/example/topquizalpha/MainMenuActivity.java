d14578f41f2386079a423582f59bfc00ba365e24
Nordine Khelfi <nordine.khelfi@gmail.com>
2019-05-05 14:41:23 +0200

    - Added player activity - Tried (un)lock topic logic

--- app/src/main/java/com/example/topquizalpha/MainMenuActivity.java
+++ app/src/main/java/com/example/topquizalpha/MainMenuActivity.java
@@ -1,11 +1,17 @@
 package com.example.topquizalpha;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.paperdb.Paper;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -14,21 +20,63 @@ public class MainMenuActivity extends AppCompatActivity {
     public ImageButton ibMuhammad;
     public ImageButton ibQuran;
     public ImageButton ibSalat;
+    public List<ImageButton> topicImageButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Paper.init(this);
+        Paper.book().write(String.valueOf(R.id.ibProphets), true);
 
         ibProphets = findViewById(R.id.ibProphets);
         ibMuhammad = findViewById(R.id.ibMuhammad);
         ibQuran = findViewById(R.id.ibQuran);
         ibSalat = findViewById(R.id.ibSalat);
+
+        topicImageButtons = Arrays.asList(
+                ibProphets,
+                ibMuhammad,
+                ibQuran,
+                ibSalat
+        );
+
+        for(ImageButton topic : topicImageButtons){
+            topic.setImageResource(Paper.book().read(String.valueOf(topic.getId()), false) ?
+                    R.drawable.ib_selector :
+                    R.drawable.mosque_bw);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Paper.book().write(String.valueOf(R.id.ibProphets), true);
+
+        for(ImageButton topic : topicImageButtons){
+            topic.setImageResource(Paper.book().read(String.valueOf(topic.getId()), false) ?
+                    R.drawable.ib_selector :
+                    R.drawable.mosque_bw);
+        }
     }
 
     public void onTopicClick(View view) {
-        Intent intent = new Intent(MainMenuActivity.this, PlayerActivity.class);
-        intent.putExtra(TOPIC_EXTRA_KEY, view.getId());
-        startActivity(intent);
+        if (Paper.book().read(String.valueOf(view.getId()), false)) {
+            Intent intent = new Intent(MainMenuActivity.this, PlayerActivity.class);
+            intent.putExtra(TOPIC_EXTRA_KEY, view.getId());
+            startActivity(intent);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This topic is locked")
+                    .setIcon(R.drawable.ic_mecca)
+                    .setMessage("You must reach 100% of good answers at the previous topic first.")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    .create().show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/topquizalpha/PlayerActivity.java
@@ -0,0 +1,87 @@
+package com.example.topquizalpha;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import io.paperdb.Paper;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        final EditText etPlayerName = findViewById(R.id.etName);
+        final Button bPlay = findViewById(R.id.bPlay);
+
+        int buttonId = getIntent().getIntExtra(MainMenuActivity.TOPIC_EXTRA_KEY, 0);
+
+        switch (buttonId) {
+            case R.id.ibProphets:
+                Toast.makeText(this, "Prophets", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ibMuhammad:
+                Toast.makeText(this, "SAWS", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ibQuran:
+                Toast.makeText(this, "Quran", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ibSalat:
+                Toast.makeText(this, "Salat", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        etPlayerName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(count > 0) {
+                    bPlay.setEnabled(true);
+                }
+                else {
+                    bPlay.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        bPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(etPlayerName.getText().toString()){
+                    case "SAWS":
+                        Paper.book().write(String.valueOf(R.id.ibMuhammad), true);
+                        Toast.makeText(PlayerActivity.this, "Congrats !", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Salât":
+                        Paper.book().write(String.valueOf(R.id.ibSalat), true);
+                        Toast.makeText(PlayerActivity.this, "Congrats !", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Quran":
+                        Paper.book().write(String.valueOf(R.id.ibQuran), true);
+                        Toast.makeText(PlayerActivity.this, "Congrats !", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "reset":
+                        Paper.book().destroy();
+                        Toast.makeText(PlayerActivity.this, "Reset.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+}
